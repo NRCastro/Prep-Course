553044c00c9af4914035ccbc644ad0d6dc1ad01e
NRCastro <nadiacastro_ar@yahoo.com.ar>
2022-08-23 20:35:47 -0300

    homework V

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -1,19 +1,50 @@
 // No cambies los nombres de las funciones.
 
-function crearUsuario() {
+function crearUsuario() {/*
   // Crea una Clase de ES6 o una función constructor llamada "Usuario"
   // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
   // En el `contructor`, define el usuario, el nombre, el email y la contraseña
   // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
-  // Tu código:
+  // Tu código:*/
+  function Usuario(opciones){ /* clase con funciones*/
+   
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.password = opciones.password;
+  }
+  Usuario.prototype.saludar = function () {
+    return 'Hola, mi nombre es ' + this.nombre ; // hola mi nombre es 'el q le pasamos'
+    // tmb podria haber sido  return `hola, mi nombre es ${this.nombre}`
+
+    //Clase ES6 seria
+    /* class Usuario {
+        Constructor(opciones){
+          this.usuario = opciones.usuario;
+          this.nombre = opciones.nombre;
+          this.email = opciones.email;
+          this.password = opciones.password;
+          
+        }
+        tmb x ejemplp podriamos poner el saludar aca adentro
+        saludar = function (){
+           return 'Hola, mi nombre es ' + this.nombre ;
+        }
+    }*/
+  }
+  return Usuario; 
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function () {
+    return 'Hello World!'
+  }
+
 }
 
 function agregarStringInvertida() {
@@ -22,6 +53,15 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+  String.prototype.reverse = function () {
+    var palabraInvertida = '';
+    for (var i = this.length - 1; i >= 0; i--){
+      palabraInvertida += this[i]; // igual q decir palabraInvertida = palabraInvertida + this[i]
+  
+    }
+    return palabraInvertida; //tmb otra forma hubiese sido return this.split("").reverse().join("");
+ 
+  }
 }
 
 // ---------------------------------------------------------------------------//
@@ -36,21 +76,43 @@ function agregarStringInvertida() {
     //  }
 
   class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
+    constructor(nombre, apellido, edad, domicilio) {
       // Crea el constructor:
+     
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.domicilio = domicilio;
+        
+      }
+      //tmb x ejemplp podriamos poner el saludar aca adentro
+      detalle(){
+         return {
+          Nombre : this.nombre,
+          Apellido : this.apellido,
+          Edad : this.edad,
+          Domicilio : this.domicilio,
+      }
 
     }
-}
+  }
 
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  return new Persona (nombre, apellido, edad, dir); // con el new creamos una nuvea instancia de persona
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  Persona.prototype.datos = function(nombre, edad){
+    return `${this.nombre}, ${this.edad} años` ;
+    // o esta forma tmb nos sirve return this.nombre + ', ' + this.edad + ' años' ;
+
+  }
+
 }
   
 
